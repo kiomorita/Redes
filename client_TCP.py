1af7e678f8a2f02548cd7fcf91eb720c6c68089a
Daniel Akio Morita <47609743+kiomorita@users.noreply.github.com>
2019-04-20 00:24:53 -0300

    Update client_TCP.py

--- client_TCP.py
+++ client_TCP.py
@@ -4,14 +4,15 @@ import socket
 
 server_name = 'localhost'
 server_port = 12000
-
-client_socket = socket.socket(socket.AF_INET,
-                              socket.SOCK_STREAM)
+#Cria socket do cliente. AF_INET declara IPv4 e SOCK_STREAM indica socket TCP
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#Essa linha está criando socket para handshake. Parametros em connect identificam endereço do lado do servidor da conexão
 client_socket.connect((server_name, server_port))
-
+#Realizada apresentação em 3 vias e estabelecido TCP
 sentence = input('Input lowercase sentence:')
+#Envia cadeia para socket 
 client_socket.send(sentence.encode())
-
+#cliente espera receber bytes de volta
 modified_sentence = client_socket.recv(1024)
 print('From Server: {}'.format(modified_sentence.decode()))
 
