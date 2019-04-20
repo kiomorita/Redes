c0db1efa1937b7b5ba07fa9fa3aa93f813ce7d6d
Daniel Akio Morita <47609743+kiomorita@users.noreply.github.com>
2019-04-20 00:18:29 -0300

    Update server_UDP.py

--- server_UDP.py
+++ server_UDP.py
@@ -14,16 +14,16 @@ def signal_handler(signal, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 server_port = 12000
-server_socket = socket.socket(socket.AF_INET,
-                              socket.SOCK_DGRAM)
-server_socket.setsockopt(socket.SOL_SOCKET,
-                         socket.SO_REUSEADDR,
-                         1)
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+#Designa porta 12000 ao socket do servidor
 server_socket.bind(('', server_port))
 print('The server is ready to receive')
 
 while True:
+    #Dado chegou, guarda em message e endereço em client_adress
     message, client_address = server_socket.recvfrom(2048)
     print('Message received: {}'.format(message.decode()))
     modified_message = message.upper()
+    #Anexa endereço do cliente a mensagem em letras maiúsculas, enviando para socket do servidor
     server_socket.sendto(modified_message, client_address)
