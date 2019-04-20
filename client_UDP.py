53f3629876c69b3525eba8cde08560ba7f39eb94
Daniel Akio Morita <47609743+kiomorita@users.noreply.github.com>
2019-04-20 00:11:36 -0300

    Update client_UDP.py

--- client_UDP.py
+++ client_UDP.py
@@ -1,16 +1,17 @@
 """simple UDP client."""
 
 import socket
-
-server_name = 'localhost'
+#Def do hostname e da porta
+server_name = 'localhost' 
 server_port = 12000
-
-client_socket = socket.socket(socket.AF_INET,
-                              socket.SOCK_DGRAM)
-
+#cria socket cliente. AF_INET fala que é IPv4 - SOCK_DGRAM é socket UDP
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+#Input da mensagem
 message = input('Input lowercase sentence: ')
-
+#sendto acrescenta endereço do destino - server name, server port - envia pacote
 client_socket.sendto(message.encode(), (server_name, server_port))
+#dados que vem da internet vão em modified e endereço de origem em server_adress
+#não precisa pois temos desde o inicio, mas...
 modified_message, server_address = client_socket.recvfrom(2048)
 
 
